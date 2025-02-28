efd34f5f51a6747ce6580f585c05f28d0b1c65c8
Brandon Ramirez <ramireb3@wwu.edu>
2025-02-28 11:36:09 -0800

    refactor

--- 42/media/lua/client/BarefootMoodle.lua
+++ 42/media/lua/client/BarefootMoodle.lua
@@ -4,10 +4,24 @@ print("Barefoot Moodle Loaded.")
 MF.createMoodle('Barefoot')
 MF.createMoodle('TornShoe')
 
+local function getPlayerShoeCondition(player)
+    if not player then return nil end  -- Ensure player exists
 
-local function OnClothingUpdated(character)
+    local shoes = player:getClothingItem_Feet()
+
+    if shoes then
+        return shoes:getCondition()  -- Return the condition of the shoes
+    else
+        return nil  -- Player is not wearing shoes
+    end
+end
+
+local function OnClothingUpdated(player)
     --MF.getMoodle('Barefoot',player:getPlayerNum()):setValue(0.0)
-    print(player:getClothingItemFeet())
+    shoes = getPlayerShoeCondition()
+    if shoes then
+        print(shoes)
+    end
 end
 
 Events.OnClothingUpdated.Add(OnClothingUpdated)
