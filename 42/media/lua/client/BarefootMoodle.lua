b663fcb09ad1a1a20e8f9b68129fe3dfdda9b779
Brandon Ramirez <ramireb3@wwu.edu>
2025-02-28 01:16:49 -0800

    common placeholder

--- 42/media/lua/client/BarefootMoodle.lua
+++ 42/media/lua/client/BarefootMoodle.lua
@@ -4,7 +4,7 @@ print("Barefoot Moodle Loaded.")
 MF.createMoodle('Barefoot')
 
 local function OnNewGame(player)
-    MF.getMoodle('Barefoot',playerNum):setValue(0.0)
+    MF.getMoodle('Barefoot',player:getPlayerNum()):setValue(0.0)
 end
 
 Events.OnNewGame.Add(OnNewGame)
