d3a0a798969127ac5613a49f551fba5882011b1e
Brandon Ramirez <br@ndonramirez.com>
2025-02-28 13:29:19 -0800

    Version 1.0

--- 42/media/lua/client/BarefootMoodle.lua
+++ 42/media/lua/client/BarefootMoodle.lua
@@ -1,27 +1,43 @@
 require "MF_ISMoodle"
-print("Barefoot Moodle Loaded.")
+print("[BarefootMoodle] Loaded!")
 
-MF.createMoodle('Barefoot')
-MF.createMoodle('TornShoe')
+MF.createMoodle("Barefoot")
+MF.createMoodle("TornShoe")
 
 local function getPlayerShoeCondition(player)
-    if not player then return nil end  -- Ensure player exists
-
     local shoes = player:getClothingItem_Feet()
 
     if shoes then
-        return shoes:getCondition()  -- Return the condition of the shoes
+        return shoes:getCurrentCondition()  -- Return the condition of the shoes
     else
         return nil  -- Player is not wearing shoes
     end
 end
 
-local function OnClothingUpdated(player)
-    --MF.getMoodle('Barefoot',player:getPlayerNum()):setValue(0.0)
-    shoes = getPlayerShoeCondition()
+local function UpdateMoodles(player)
+
+    local shoes = getPlayerShoeCondition(player)
     if shoes then
-        print(shoes)
+        MF.getMoodle('Barefoot',player:getPlayerNum()):setValue(0.5)
+
+        if 15 < shoes and shoes <= 20 then
+            MF.getMoodle('TornShoe',player:getPlayerNum()):setValue(0.4)
+        elseif 10 < shoes and shoes <= 15 then
+            MF.getMoodle('TornShoe',player:getPlayerNum()):setValue(0.3)
+        elseif 5 < shoes and shoes <= 10 then
+            MF.getMoodle('TornShoe',player:getPlayerNum()):setValue(0.2)
+        elseif 0 < shoes and shoes <= 5 then
+            MF.getMoodle('TornShoe',player:getPlayerNum()):setValue(0.1)
+        elseif shoes <= 0 then
+            MF.getMoodle('TornShoe',player:getPlayerNum()):setValue(0.5)
+            MF.getMoodle('Barefoot',player:getPlayerNum()):setValue(0.0)
+        else
+            MF.getMoodle('TornShoe',player:getPlayerNum()):setValue(0.5)
+        end
+    else
+        MF.getMoodle('TornShoe',player:getPlayerNum()):setValue(0.5)
+        MF.getMoodle('Barefoot',player:getPlayerNum()):setValue(0.0)
     end
 end
 
-Events.OnClothingUpdated.Add(OnClothingUpdated)
+Events.OnPlayerUpdate.Add(UpdateMoodles)
